2c46f7796e54d27a5f817cad8aa3b49ad0e7edc8
Hannah Murphy <hamurphy@microsoft.com>
2020-02-13 18:56:04 -0800

    customization updates

---
+++ src/StackAdmin/Azs.Compute.Admin/custom/VMExtension.cs
@@ -0,0 +1,25 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.ComputeAdmin.Models.Api20151201Preview
+{
+    using System;
+
+    public partial class VMExtension
+    {
+        
+      /*  private string _type = null;
+
+        public string Type 
+        { 
+            get {
+                if (_type == null)
+                {
+                    var idArray = this.Id.Split('/');
+                    _type = idArray[Array.IndexOf(idArray, "types")+1];
+                }
+
+                return _type;
+            } 
+        }
+        */
+        
+    }
+}
